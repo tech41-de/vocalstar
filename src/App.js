17dc48face1c1a194e7a9fd03e164d63ecb6057a
tech41-de <mathias.dietrich@tech41.de>
2023-10-03 10:34:33 +0200

    dep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <center>
+      <img src="/logo.png" alt="Vocalstar" width="760" height="760"/>
+      <br/>
+       <br/>
+      <a href="mailto:info@vocalstar.app">info@vocalstar.app</a>
+        <br/> <br/>
+      <div class="me" id="me" />
+        <br/>
+    </center>
     </div>
   );
 }
