206b608f6c440a218df72f72ad7363c8f0e2696a
tech41-de <mathias.dietrich@tech41.de>
2023-10-03 10:08:46 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import logo from './img/vocalstar_logo.png';
 
 function App() {
   return (
     <div className="App">
-     <center>
-      <img src={logo} alt="Vocalstar" width="760" height="760"/>
-      <br/>
-       <br/>
-      <a href="mailto:info@vocalstar.app">info@vocalstar.app</a>
-        <br/> <br/>
-        <br/>
-    </center>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
