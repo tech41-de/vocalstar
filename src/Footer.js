2805c779931bc1ed56307fff8e8c3670ea8ce164
tech41-de <mathias.dietrich@tech41.de>
2024-03-13 15:22:05 +0100

    new imaes

--- src/Footer.js
+++ src/Footer.js
@@ -34,7 +34,7 @@ export default function FooterLayout(props) {
        <Spacer/>
        <Divider/>
       <HStack>
-        <div class="copy"><a class="copy" href="https://tech41.de" target="_blank">©2023 TECH41 GmbH</a></div><Spacer/>
+        <div class="copy"><a class="copy" href="https://tech41.de" target="_blank">©2024 TECH41 GmbH</a></div><Spacer/>
         <Link class="copy" href="https://vocalstar.app">Home</Link><Spacer/>
         <Link class="copy" href="impressum">Impressum</Link><Spacer/>
         <Link class="copy" href="policy">Privacy Policy</Link><Spacer/>
